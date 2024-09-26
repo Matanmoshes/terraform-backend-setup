00af878782a0653f4d63e73b6fc6ec73e7652dab
Matanmoshes <matanmoshes66@gmail.com>
2024-09-27 00:32:17 +0300

    sre-monitoring

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "region" {
 variable "bucket_name" {
     description = "Unique S3 bucket name for Terraform state."
     type        = string
-    default = "terraform-backend-bucket-ioioio6"
+    default = "terraform-backend-bucket-ioioio7"
 }
 
 variable "dynamodb_table_name" {
