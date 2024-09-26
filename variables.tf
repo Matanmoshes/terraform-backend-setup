77f282be9a6f4c4825e430f846e363548a15d2a2
Matanmoshes <matanmoshes66@gmail.com>
2024-09-27 01:36:18 +0300

    new version

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "region" {
 variable "bucket_name" {
     description = "Unique S3 bucket name for Terraform state."
     type        = string
-    default = "terraform-backend-bucket-ioioio7"
+    default = "terraform-backend-bucket-ioioio8"
 }
 
 variable "dynamodb_table_name" {
