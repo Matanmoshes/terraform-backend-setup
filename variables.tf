48dd6f956d09a56c5ffee9e4264215f149c241e9
Matanmoshes <matanmoshes66@gmail.com>
2024-09-27 15:45:27 +0300

    setup alrting

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "region" {
 variable "bucket_name" {
     description = "Unique S3 bucket name for Terraform state."
     type        = string
-    default = "terraform-backend-bucket-ioioio8"
+    default = "terraform-backend-bucket-ioioio9"
 }
 
 variable "dynamodb_table_name" {
