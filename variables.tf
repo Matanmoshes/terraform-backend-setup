f17463ce9ab447c9da821c7f1f125af9171596a1
Matanmoshes <matanmoshes66@gmail.com>
2024-10-02 10:35:05 +0300

    backend for jenkins assigment

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "region" {
 variable "bucket_name" {
     description = "Unique S3 bucket name for Terraform state."
     type        = string
-    default = "terraform-backend-bucket-ioioio18"
+    default = "terraform-backend-bucket-ioioio19"
 }
 
 variable "dynamodb_table_name" {
